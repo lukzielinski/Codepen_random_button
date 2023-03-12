e8bf222c1d8e73149380cad5113d24a9a796e9af
ReQ <mrzielony22@gmail.com>
2023-03-12 16:03:22 +0100

    Fixes for styles

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const btn = document.getElementById('button');
+const character = document.getElementById('character');
 let isVisible = true;
 
 btn.addEventListener('click', () => {
     isVisible = false;
     btn.style.transition = 'opacity 1s';
     btn.style.opacity = '0';
+    character.style.display = 'block';
     setTimeout(() => {
         btn.style.opacity = '1';
-    }, 5000);
+        character.style.display = 'none';
+    }, 6000);
 });
